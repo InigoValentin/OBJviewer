f436b2c67995568220c692661971cbe51421d99e
Iñigo Valentin <seavenois@gmail.com>
2014-04-20 22:05:35 +0200

    Implemented file selector (but it doesn't load the model yet).

--- OBJViewer/src/com/seavenois/obj/ExplorerActivity.java
+++ OBJViewer/src/com/seavenois/obj/ExplorerActivity.java
@@ -18,6 +18,7 @@ import android.widget.TextView;
 
 public class ExplorerActivity extends Activity{
 
+	boolean rootView;
 	LinearLayout llContent;
 	ProgressBar pbReload;
 	ImageView ivReload;
@@ -56,9 +57,18 @@ public class ExplorerActivity extends Activity{
 			}
 		});
 		
+		loadFolders();
+	}
+	
+	private void loadFolders(){
+		//Set variable
+		rootView = true;
+		
+		//Clean layout
+		llContent.removeAllViews();
+		
 		//Read database
 		SQLiteDatabase db = SQLiteDatabase.openOrCreateDatabase(this.getFilesDir().getPath() + "/obj.sqlite", null);
-		File file;
 		Cursor cur;
 		
 		//Search for the ones in db and delete the ones that dont exist
@@ -77,7 +87,13 @@ public class ExplorerActivity extends Activity{
 				tvCount.setText(cur.getString(0) + getString(R.string.explorer_file_plural));
 			tvName.setText(cur.getString(1).substring(cur.getString(1).lastIndexOf("/")));
 			tvPath.setText(cur.getString(1).substring(0, cur.getString(1).lastIndexOf("/")));
-			//TODO: Add touch listener to row
+			final String str = cur.getString(1);
+			row.setOnClickListener(new OnClickListener(){
+				@Override
+				public void onClick(View v) {
+					openFolder(str);
+				}
+			});
 			llContent.addView(row);
 			cur.moveToNext();
     	}
@@ -86,7 +102,60 @@ public class ExplorerActivity extends Activity{
 		db.close();
 	}
 	
+	private void openFolder(String path){
+		//Clear layout
+		llContent.removeAllViews();
+		
+		//Read database
+		SQLiteDatabase db = SQLiteDatabase.openOrCreateDatabase(this.getFilesDir().getPath() + "/obj.sqlite", null);
+		Cursor cur;
+		
+		//Search for the ones in db and delete the ones that dont exist
+		cur = db.rawQuery("SELECT file, path, fname, size, vertices, faces, mtl, materials, name FROM model WHERE path = '" + path + "';", null);
+		View row = null;
+		String str;
+		TextView tvName, tvDetail, tvMtl;
+		cur.moveToFirst();
+		while (cur.isAfterLast() == false){
+			row = getLayoutInflater().inflate(R.layout.row_file, null);
+			tvName = (TextView) row.findViewById(R.id.tvFileName);
+			tvDetail = (TextView) row.findViewById(R.id.tvFileDetails);
+			tvMtl = (TextView) row.findViewById(R.id.tvFileMtl);
+			tvName.setText(cur.getString(2));
+			if (cur.getInt(6) == 1){
+				tvMtl.setVisibility(View.VISIBLE);
+				str = String.format(getString(R.string.explorer_details_mtl), cur.getString(3), cur.getString(4), cur.getString(5), cur.getString(7));
+			}
+			else{
+				tvMtl.setVisibility(View.GONE);
+				str = String.format(getString(R.string.explorer_details_nomtl), cur.getString(3), cur.getString(4), cur.getString(5));
+			}
+			tvDetail.setText(str);
+			final String st = cur.getString(0);
+			row.setOnClickListener(new OnClickListener(){
+				@Override
+				public void onClick(View v) {
+					//TODO
+					Log.d("Selectef file", st);
+				}
+			});
+			llContent.addView(row);
+			cur.moveToNext();
+		}
+		
+		cur.close();
+		db.close();
+	}
+	
 	public void reload(){
 		new Reload(this, ivReload, pbReload).execute();
+	}
+	
+	@Override
+	public void onBackPressed() {
+		if (rootView != false)
+			loadFolders();
+		else
+			super.onBackPressed();
 	}
 }
